3f4559749f17746bea468a6d27519f63374b950b
Riquelme <riquelmestayler@gmail.com>
2025-04-29 23:28:42 -0300

    Done new authServiceTest

--- backend/src/services/authService.test.ts
+++ backend/src/services/authService.test.ts
@@ -1,36 +1,70 @@
-import { describe, test, expect, beforeAll, afterAll } from '@jest/globals';
-import { newUser } from '../types/modelsType';
-import * as authService from './authService';
-import { deleteUserByEmail } from '../models/userModel';
+import { describe, test, expect, afterAll } from '@jest/globals'
+import { User } from '@prisma/client'
+import * as authService from '../services/authService'
+import { createStudentUser } from '../utils/objectTest'
+import { createStripeCustomer } from '../utils/stripe'
+import { prisma } from '../database/prisma'
+import bcrypt from 'bcrypt'
 
 
-describe('Should test all function in authService', () => {
+describe("Should test all functions on service", () => {
+    let user: User
+    let userExisting: User
 
-    const user: newUser = {
-        cpf: '12345678901',
-        email: 'teste@gmail.com',
-        name: 'teste',
-        password: 'Teste123!',
-        type: 'Student'
-    }
-
-    test('Should test create user', async () => {
-        const newUser = await authService.signUp(user)
-
-        expect(newUser).toHaveProperty('id')
-        expect(newUser.name).toBe('teste')
+    beforeAll(async () => {
+        userExisting = await prisma.user.create({ data: await createStudentUser() })
     })
 
-    test('Should test user login', async () => {
-        const userLogged = await authService.signIn(user.email, 'Teste123!')
+    test("Should register a new user", async () => {
+        let customer = await createStripeCustomer({ email: 'riquelmeadmin@gmail.com', name: 'Riquelme Admin' })
 
-        expect(userLogged).toHaveProperty('id')
-        expect(userLogged?.email).toBe(user.email)
+        user = await authService.signUp({
+            email: 'riquelmeadmin@gmail.com',
+            cpf: '06955734112',
+            name: 'Riquelme Admin',
+            password: 'Senhateste123!',
+            stripeCustomerId: customer.id,
+            type: 'Admin'
+        })
+
+        expect(user).toHaveProperty('id')
+        expect(user.type).toBe('Admin')
+        expect(user.stripeCustomerId).toBe(customer.id)
+    })
+
+    test("Shouldn't register user because exist", async () => {
+        await expect(() => {
+            return authService.signUp({
+                cpf: userExisting.cpf,
+                email: userExisting.email,
+                name: userExisting.name,
+                password: userExisting.password,
+                type: user.type
+            })
+        }).rejects.toThrow('User has exist')
+    })
+
+    test("Should login ", async () => {
+        const userLogin = await authService.signIn(user.email, 'Senhateste123!')
+
+        expect(userLogin?.name).toBe(user.name)
+        expect(userLogin?.email).toBe(user.email)
+    })
+
+    test("Shouldn't login because password is wrong", async () => {
+        await expect(() => {
+            return authService.signIn(user.email, 'senhatotalmenteerrada123!')
+        }).rejects.toThrow('Wrong password')
+    })
+
+    test("Shouldn't login because email not exist", async () => {
+        await expect(() => {
+            return authService.signIn('emailfalso@gmail.com', 'Senhateste123!')
+        }).rejects.toThrow('Email not exist')
     })
 
     afterAll(async () => {
-        await deleteUserByEmail(user.email)
+        await prisma.user.delete({ where: { id: user.id } })
+        await prisma.user.delete({ where: { id: userExisting.id } })
     })
-
-
 })
--- backend/src/utils/objectTest.ts
+++ backend/src/utils/objectTest.ts
@@ -1,5 +1,53 @@
-import { createStripeCustomer } from "./stripe"
+import { User } from "@prisma/client";
+import { createStripeCustomer } from "./stripe";
+
+
 
 export const createAdminUser = async () => {
-    const customer = await createStripeCustomer
+    const customer = await createStripeCustomer({ email: 'riquelmeadmin@gmail.com', name: 'Riquelme Admin' })
+
+    const adminUser: User = {
+        id: 1,
+        email: 'riquelmeadmin@gmail.com',
+        cpf: '06955734112',
+        name: 'Riquelme Admin',
+        password: 'Senhateste123!',
+        stripeCustomerId: customer.id,
+        type: 'Admin'
+    }
+
+    return adminUser
 }
+
+export const createTeacherUser = async () => {
+    const customer = await createStripeCustomer({ email: 'riquelmeteacher@gmail.com', name: 'Riquelme Teacher' })
+
+    const teacherUser: User = {
+        id: 2,
+        email: 'riquelmeteacher@gmail.com',
+        cpf: '06955734114',
+        name: 'Riquelme Teacher',
+        password: 'Senhateste123!',
+        stripeCustomerId: customer.id,
+        type: 'Teacher'
+    }
+
+    return teacherUser
+}
+
+export const createStudentUser = async () => {
+    const customer = await createStripeCustomer({ email: 'riquelmesenna577@gmail.com', name: 'Riquelme Senna' })
+
+    const studentUser: User = {
+        id: 3,
+        email: 'riquelmesenna577@gmail.com',
+        cpf: '06955734113',
+        name: 'Riquelme Senna',
+        password: 'Senhateste123!',
+        stripeCustomerId: customer.id,
+        type: 'Student'
+    }
+
+    return studentUser
+}
+
