fb7332a4381fb566d58054c42d94161b3924dbac
Riquelme <riquelmestayler@gmail.com>
2025-05-22 00:03:00 -0300

    Done method register frontEnd first step

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,6 +21,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(helmet());
 server.use(express.static('public'));
+server.use(cors({ exposedHeaders: 'Authorization' }))
 
 const port = process.env.PORT || 4000
 
